16f47d1f9a0d10557a95dc6a4c7a16e17410a555
okawaffles <okawaffles@gmail.com>
2022-11-03 18:31:07 -0500

    fix issue in d.ts (constructor for name arg)

--- index.d.ts
+++ index.d.ts
@@ -4,9 +4,10 @@ export function error(name: string, text: string): void;
 
 // i have no idea whether this is right or not, but it doesn't give me any errors, so:
 export class Logger{
+    constructor(name: string);
     name: string
     setName(name: string): void;
     info(text: string): void;
     warn(text: string): void;
     error(text: string): void;
-};
+}
